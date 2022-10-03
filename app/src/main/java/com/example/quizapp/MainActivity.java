7285aa7efc0d25f298dcfd4fc83c53e0a135a34f
Hara <harachung90@gmail.com>
2022-10-03 20:17:23 +1100

    Second commit. created onClickListener

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button answer_a, answer_b, answer_c, answer_d;
+    Button submitBtn;
+
+    int score = 0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        answer_a = findViewById(R.id.answer_a);
+        answer_b = findViewById(R.id.answer_b);
+        answer_c = findViewById(R.id.answer_c);
+        answer_d = findViewById(R.id.answer_d);
+
+        submitBtn = findViewById(R.id.submit_btn);
+
+        answer_a.setOnClickListener(this);
+        answer_b.setOnClickListener(this);
+        answer_c.setOnClickListener(this);
+        answer_d.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions: " + totalQuestion);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+           "Which company owns the android?",
+           "Which one is not the programming language?",
+           "What is the largest video sharing platform?" ,
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Facebook", "Whatsapp", "Instagram", "YouTube"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "YouTube"
+    };
+}
