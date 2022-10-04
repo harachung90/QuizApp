f451d05820acb668a252f9d1f9ac2f4530747d62
Hara <harachung90@gmail.com>
2022-10-04 19:23:27 +1100

    Third commit. App complete

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,10 +42,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submitBtn.setOnClickListener(this);
 
         totalQuestionsTextView.setText("Total questions: " + totalQuestion);
+
+        loadNewQuestion();
     }
 
     @Override
     public void onClick(View view) {
 
+        answer_a.setBackgroundColor(Color.WHITE);
+        answer_b.setBackgroundColor(Color.WHITE);
+        answer_c.setBackgroundColor(Color.WHITE);
+        answer_d.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if (clickedButton.getId() == R.id.submit_btn) {
+            if (selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])) {
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+        } else {
+            //choices button clicked
+            selectedAnswer = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+        }
+    }
+
+    void loadNewQuestion() {
+
+        if (currentQuestionIndex == totalQuestion) {
+            finishQuiz();
+            return;
+        }
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        answer_a.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        answer_b.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        answer_c.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        answer_d.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+    }
+
+    void finishQuiz() {
+        String passStatus =  "";
+        if (score > totalQuestion * 0.60) {
+            passStatus = "Passed!";
+        } else {
+            passStatus = "Failed!";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Your score is " + score + " out of " + totalQuestion)
+                .setPositiveButton("Restart", ((dialogInterface, i) -> restartQuiz()))
+                .setCancelable(false)
+                .show();
+    }
+
+    void restartQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        loadNewQuestion();
     }
 }
